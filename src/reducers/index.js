45939773cd47dca82b0523fb9765083fd41cb7ab
SuitcaseCoder <lauraruizroehrs@gmail.com>
2019-06-21 14:23:51 -0500

    edited initial state in reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,63 @@
+import * as actions from '../actions';
+
 const initialState = {
-    wordsA: [{
-        letter: 'A',
-        word: 'Apfel',
-        definition: 'Ein Obst, der von Baeume waechst.A yummy fruit that grows on trees.'
-    }, {
-        letter: 'A',
-        word: 'abnehmen',
-        definition: 'duenner zu werden. to lose weight'
-    }],
-    wordsB: [{
-        letter: 'B',
-        word: 'Baum',
-        definition: 'Eine grosse Pflanze mit vielen Blaetter. A tree.'
+    letterLists: [{
+        title: 'Letter List A',
+        words: [{
+            word: 'A Example Word 1',
+            definition: 'A Example Definition 1'
+        },{
+            word: 'A Example Word 2',
+            definition: 'A Example Definition 2'
+        }, {
+            word: 'A Example Word 3',
+            definition: 'A Example Definition 3'
+        }] 
+    },{
+        title: 'Letter List B',
+        words: [
+            {
+                word: 'B Example Word 1',
+                definition: 'B Example Definition 1'
+            }, {
+                word: 'B Example Word 2',
+                definition: 'B Example Definition 2'
+            }
+        ]
+    },{
+        title: 'Letter List C',
+        words: [
+            {
+                word: 'C Example Word 1',
+                definition: 'C Example Definition 1'
+            }, {
+                word: 'C Example Word 2',
+                definition: 'C Example Definition 2'
+            },{
+                word: 'C Example Word 3',
+                definition: 'C Example Definition 3'
+            }
+        ]
     }]
 };
+
+
+// const initialState = {
+//     words: [{
+//         letter: 'A',
+//         word: 'Apfel',
+//         definition: 'Ein Obst, der von Baeume waechst.A yummy fruit that grows on trees.'
+//     }, {
+//         letter: 'A',
+//         word: 'abnehmen',
+//         definition: 'duenner zu werden. to lose weight'
+//     }],
+//     words: [{
+//         letter: 'B',
+//         word: 'Baum',
+//         definition: 'Eine grosse Pflanze mit vielen Blaetter. A tree.'
+//     }]
+// };
 
 //help.
 export const wordsReducer = (state=initialState, action) => {
